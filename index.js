a52df13dd50d887fb835a18baee862726b3f1a78
Reyalic <95107731+Reyalic@users.noreply.github.com>
2022-06-13 18:20:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ function mergeData(users, attendances) {
         { email: 'georg@academlo.com', attendance: true }
     ]
     for (i = 0; i <= users.length-1; i++) {
-        if (users[i].email == attendances[i].email) { 
-            var allData = [{
-            name: users[i].name,
-            email: users[i].email,
-            attendance: attendances[i].attendance }]
-        }
+        let allData = [{
+        name: users[i].name,
+        email: users[i].email,
+        attendance: attendances[i].attendance }]
+
         //return allData
         console.log(allData)
     }
 }
+
 
 console.log(mergeData())
